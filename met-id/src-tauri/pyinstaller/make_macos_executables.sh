44e677dd284974177ab685e53ff04de2c6f80765
Patrik Bjärterot <patrikbjarterot47@gmail.com>
2024-05-27 11:39:56 +0200

    Update make_macos_executables.sh

--- met-id/src-tauri/pyinstaller/make_macos_executables.sh
+++ met-id/src-tauri/pyinstaller/make_macos_executables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pyinstaller ../src/metabolite.py --onefile -n metabolite-x86_64-apple-darwin
-pyinstaller ../src/metabolite.py --onefile -n metabolite-aarch-apple-darwin
+pyinstaller ../src/metabolite.py --onefile -n metabolite-aarch64-apple-darwin
 pyinstaller ../src/metabolite_for_db.py --onefile -n metabolite_for_db-x86_64-apple-darwin
 pyinstaller ../src/metabolite_for_db.py --onefile -n metabolite_for_db-aarch-apple-darwin
 pyinstaller ../src/matrix_for_db.py --onefile -n matrix_for_db-x86_64-apple-darwin
