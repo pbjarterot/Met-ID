5b5a06206f093bf7b53cff4ef1a3e574b0a81eca
Patrik Bjärterot <patrikbjarterot47@gmail.com>
2025-01-20 16:24:12 +0100

    updated build.rs

--- met-id/src-tauri/build.rs
+++ met-id/src-tauri/build.rs
@@ -1,4 +1,8 @@
 fn main() {
-    let _ = tonic_build::compile_protos("src/add_to_db/protos/streaming.proto");
+    //let _ = tonic_build::compile_protos("src/add_to_db/protos/streaming.proto");
+    tonic_build::configure()
+        .out_dir("src/add_to_db")
+        .compile_protos(&["src/add_to_db/protos/streaming.proto"], &["proto"])
+        .expect("Failed to compile proto files");
     tauri_build::build()
 }
