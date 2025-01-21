9e8b5da9dec1280756cb6420c8dfbea845e2d0a1
Patrik Bjärterot <patrikbjarterot47@gmail.com>
2025-01-21 11:07:13 +0100

    Update cargo and build

--- met-id/src-tauri/build.rs
+++ met-id/src-tauri/build.rs
@@ -1,8 +1,10 @@
 fn main() {
     //let _ = tonic_build::compile_protos("src/add_to_db/protos/streaming.proto");
+    /* 
     tonic_build::configure()
         .out_dir("src/add_to_db")
         .compile_protos(&["src/add_to_db/protos/streaming.proto"], &["proto"])
         .expect("Failed to compile proto files");
+    */
     tauri_build::build()
 }
